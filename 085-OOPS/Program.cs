8982d4d51d48541bcd822a0e49fdd5e26b79d45b
Akashkapase9988 <akashkapase9988@gmail.com>
2021-11-17 11:56:17 +0530

    Add project files.

---
+++ 085-OOPS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _085_OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
